e9624ec77c3fa67dccb29bddb0bea4c4a8e224f1
Simon Ludwig <simonludwig1995@gmail.com>
2020-03-26 15:25:49 +0100

    fix

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (<div className={"myfooter"}>
         <a href={"/impressum"}>Impressum | </a>
         <a href={"/datenschutz"}>Datenschutz | </a>
-        <a href={"/offer/message?email=info@videobakers.de"}>Kontakt | </a>
+        <a href={"/message/offer?email=info@videobakers.de"}>Kontakt | </a>
         <a href={"/faq"}>FAQ</a>
     </div>);
 };
